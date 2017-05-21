722fc88c900b18a0a4be53ca216d4061adfe54c7
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-05-21 12:45:49 +0200

    Setup for Room and Archi lifecycle.

--- app/src/main/java/com/mounacheikhna/archicomponents/ArchiApp.kt
+++ app/src/main/java/com/mounacheikhna/archicomponents/ArchiApp.kt
@@ -1,7 +1,37 @@
 package com.mounacheikhna.archicomponents
 
+import android.app.Activity
 import android.app.Application
+import android.app.Fragment
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasDispatchingActivityInjector
+import javax.inject.Inject
 
-class ArchiApp : Application() {
+class ArchiApp : Application(), HasDispatchingActivityInjector {
+
+  private var instance: ArchiApp? = null
+  private var component: AppComponent? = null
+
+  @Inject internal var dispatchingActivityInjector: DispatchingAndroidInjector<Activity>? = null
+  @Inject internal var dispatchingFragmentInjector: DispatchingAndroidInjector<Fragment>? = null
+
+  fun getAppComponent(): AppComponent {
+    return instance!!.getComponent()
+  }
+
+  fun getComponent(): AppComponent {
+    return component!!
+  }
+
+  override fun onCreate() {
+    super.onCreate()
+    instance = this
+    component = DaggerAppComponent.builder().app(this).build()
+    component!!.inject(this)
+  }
+
+  override fun activityInjector(): DispatchingAndroidInjector<Activity> {
+    return dispatchingActivityInjector!!
+  }
 
 }
